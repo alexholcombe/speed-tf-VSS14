85784ffa18f3c6deca94cd60362c2bfc80a45547
Alex O. Holcombe <aoholcombe@gmail.com>
2015-06-22 15:09:35 +1000

    doing stats for CRT/spinzter experiment

--- README.md
+++ README.md
@@ -2,6 +2,11 @@ speed-tf-VSS14
 ==============
 
 Speed limits, and relation to temporal freq limits on attentional tracking. 
+- rawdata123targets269objects
+- postVSS_13targets2349objects
+- experiment2_methodAdjust
+
+These experiments will likely be published in the same paper as the off-center and diamond-versus-circle experiments, which are analysed in a different repository: newTraj_repo/experiment-specific
 
 **doAllAnalyses.R** in **analyseExps** folder is the main analysis, which calls various other files.
 
@@ -10,3 +15,4 @@ Before that,
 top-level **data** subdirectory. It is these anonymised files that doAllAnalyses etc analyse.
 
 Seems to all be working, except maybe model limits.
+
--- analyseExps/doAllAnalyses.R
+++ analyseExps/doAllAnalyses.R
@@ -1,14 +1,17 @@
 #This file analyses anonymized data provided by "loadAnonymiseSaveData.R" in exp-specific directory
 #Working directory is set hopefully by Rproj file to directory that it's in.
 setwd("/Users/alexh/Documents/attention_tempresltn/multiple\ object\ tracking/ExperimentsWithWing/speedLimitsAndTargetLoad/allAnalysisForPosting/speed-tf-VSS14/analyseExps")
-
+require("ggplot2")
+require("plyr")
 expName="123targets269objects"
 dataDir="../data/"
+
 expName = "data123targets269objects"
 anonDataFilename = paste(dataDir,expName,".Rdata",sep="") 
 load(anonDataFilename,verbose=TRUE) #E1 #returns dat
 dat$expName = expName
 datE1 = dat
+
 expName="postVSS_13targets2349objects" 
 anonDataFname= paste(dataDir,expName,".Rdata",sep="") #data/postVSS_13targets2349objects.RData
 load(anonDataFname,verbose=TRUE) #returns dat
@@ -18,17 +21,14 @@ colsNotInE1 = setdiff(colnames(dat_13targets2349objects),colnames(datE1))
 datE1[,colsNotInE1] = -999 #dummy value
 colsNotInThisOne = setdiff(colnames(datE1),colnames(dat_13targets2349objects))
 dat_13targets2349objects[,colsNotInThisOne] = -999 #dummy value
-
-load("offCenter.RData_symbolic_link",verbose=TRUE) #symbolic link created with ln -s works, but not OSX alias
-offCenter = dat
-iv= 'speed'
-source('analyzeMakeReadyForPlot.R') #returns fitParms, psychometrics, and function calcPctCorrThisSpeed
-source('plotIndividDataWithPsychometricCurves.R')
-
-
-
-
 dat = rbind(dat_13targets2349objects,datE1)
+
+#Analysed in separate repository
+# load("offCenter.RData_symbolic_link",verbose=TRUE) #symbolic link created with ln -s works, but not OSX alias
+# offCenter = dat
+# iv= 'speed'
+# source('analyzeMakeReadyForPlot.R') #returns fitParms, psychometrics, and function calcPctCorrThisSpeed
+# source('plotIndividDataWithPsychometricCurves.R')
 
 dat$tf = dat$speed*dat$numObjects
 for (iv in c("speed","tf")) {
@@ -55,16 +55,47 @@ meanThreshold<-function(df) {
   return(df)
 }  
 factorsPlusSubject=c("numObjects","subject","direction","ecc","device") 
-
+#Document that there is no effect of direction of motion
 E2threshes<- ddply(E2, factorsPlusSubject,meanThreshold) #average over trialnum,startSpeed
+g=ggplot(E2threshes, aes(x=direction, y=thresh, color=device, shape=factor(ecc)))
+g<-g + xlab('Distractors')+ylab('threshold speed (rps)')
+dodgeAmt=0.35
+SEerrorbar<-function(x){ SEM <- sd(x) / (sqrt(length(x))); data.frame( y=mean(x), ymin=mean(x)-SEM, ymax=mean(x)+SEM ) }
+#g<-g+ geom_point()
+g<-g+ stat_summary(fun.data="SEerrorbar",geom="point",size=2.5,position=position_dodge(width=dodgeAmt))
+#g<-g+ stat_summary(fun.y=mean,geom="point",size=2.5,position=position_dodge(width=dodgeAmt))
+g<-g+stat_summary(fun.data="SEerrorbar",geom="errorbar",width=.25,position=position_dodge(width=dodgeAmt)) 
+g=g+theme_bw() #+ facet_wrap(~direction)
+g<-g+theme(panel.grid.minor=element_blank(),panel.grid.major=element_blank())# hide all gridlines.
+g
 
-#then plot
+#Do statistics on direction of motion
+#First collapse within subject for simple t-test and calculate means
+CRT<- subset(E2,device=="CRT") #only in CRT experiment was direction of motion tested
+factors<-c("subject","direction") 
+CRTthreshes<- ddply(E2,factors,meanThreshold) #average over trialnum,startSpeed
+CCW=subset(CRTthreshes,direction=="CCW")$thresh
+CW=subset(CRTthreshes,direction=="CW")$thresh
+t<- t.test(x=CCW,y=CW,paired=TRUE)
+paste("Mean CCW=",round(mean(CCW),2),"CW=",round(mean(CW),2),
+      "t=",round(t$statistic,3),"p=",round(t$p.value),3)
+#Counter-clockwise is faster
+options(contrasts=c("contr.sum","contr.poly")) #http://blog.gribblelab.org/2009/03/09/repeated-measures-anova-using-r/
+#This matters sometimes (not always). If you don’t do it, your sum of squares calculations may not match what you get, for example, in SPSS, or in many common textbooks on ANOVA (e.g. Maxwell & Delaney). See [obtaining the same ANOVA results in R as in SPSS - the difficulties with Type II and Type III sums of squares] for a more detailed account of why this is.
+aov.out = aov(thresh ~ direction + Error(subject/(direction)), data=CRT)
+
+#Then do ANOVA, make sure no interaction with eccentricity or anything
+
+factorsPlusSubject=c("numObjects","subject","ecc","device") 
+E2threshes<- ddply(E2, factorsPlusSubject,meanThreshold) #average over trialnum,startSpeed
+#plot
 tit<-"E2threshesSpeed"
 quartz(title=tit,width=2.8,height=2.9) #create graph of thresholds
 g=ggplot(E2threshes, aes(x=numObjects-1, y=thresh, color=device, shape=factor(ecc)))
 g<-g + xlab('Distractors')+ylab('threshold speed (rps)')
 dodgeAmt=0.35
 SEerrorbar<-function(x){ SEM <- sd(x) / (sqrt(length(x))); data.frame( y=mean(x), ymin=mean(x)-SEM, ymax=mean(x)+SEM ) }
+#g<-g+ geom_point()
 g<-g+ stat_summary(fun.data="SEerrorbar",geom="point",size=2.5,position=position_dodge(width=dodgeAmt))
 #g<-g+ stat_summary(fun.y=mean,geom="point",size=2.5,position=position_dodge(width=dodgeAmt))
 g<-g+stat_summary(fun.data="SEerrorbar",geom="errorbar",width=.25,position=position_dodge(width=dodgeAmt)) 
