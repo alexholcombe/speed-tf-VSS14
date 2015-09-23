918fdb3092b4d06c55bbb8266ba6770f85a59e06
Alex O. Holcombe <aoholcombe@gmail.com>
2015-09-23 16:35:00 +1000

    about to migrate later paper prettifying

--- analyseExps/plotIndividDataWithPsychometricCurves.R
+++ analyseExps/plotIndividDataWithPsychometricCurves.R
@@ -1,6 +1,15 @@
 #Variables expected:
 #dat
 #iv - "tf" or "speed"
+themeAxisTitleSpaceNoGridLinesLegendBox = theme_classic() + #Remove gridlines, show only axes, not plot enclosing lines
+  theme(axis.line = element_line(size=.3, color = "grey"), 
+        axis.title.y=element_text(vjust=0.24), #Move y axis label slightly away from axis
+        axis.title.x=element_text(vjust=.10), #Move x axis label slightly away from axis
+        legend.key = element_blank(), #don't put boxes around legend bits
+        legend.background= element_rect(color="grey90"), #put big light grey box around entire legend
+        panel.background = element_rect(fill = "transparent",colour = NA),
+        plot.background = element_rect(fill = "transparent",colour = NA)   )
+
 if (!("speed" %in% colnames(psychometrics))) { #psychometrics must have been fit to tf
   stopifnot("tf" %in% colnames(psychometrics)) #confirm my interpretation that tf was fit
   psychometrics$speed = psychometrics$tf / psychometrics$numObjects #because always plot them in terms of speed
@@ -12,18 +21,17 @@ plotIndividDataAndCurves <- function(df,psychometricCurves) {
   g=ggplot(data= df,aes(x=speed,y=correct,color=factor(numTargets),shape=factor(separatnDeg)))
   g=g+stat_summary(fun.y=mean,geom="point", position=position_jitter(w=0.04,h=0),alpha=.95)
   g=g+facet_wrap(separatnDeg ~ subject,ncol=8)+theme_bw()
-  g
-  
+
   #can't do this right now because depends on criterion
   # thisThreshes<- subset(threshesThisNumeric, exp==1)
   # threshLines <- ddply(thisThreshes,factorsPlusSubject,threshLine)
   # g<-g+ geom_line(data=threshLines,lty=3,size=0.9)  #,color="black") #emphasize lines so can see what's going on
-  
   g<-g+ coord_cartesian( xlim=c(xLims[1],xLims[2]), ylim=yLims ) #have to use coord_cartesian here instead of naked ylim()
   g
   g=g+geom_line(data=psychometricCurves)
   g=g+ geom_hline(mapping=aes(yintercept=chanceRate),lty=2)  #draw horizontal line for chance performance
   g=g+xlab('Speed (rps)')+ylab('Proportion Correct')
+  g<-g+ themeAxisTitleSpaceNoGridLinesLegendBox
   g=g+theme(panel.grid.minor=element_blank(),panel.grid.major=element_blank())# hide all gridlines.
   g <- g+ theme(axis.title.y=element_text(size=12,angle=90),axis.text.y=element_text(size=10),axis.title.x=element_text(size=12),axis.text.x=element_text(size=10))
   g<-g+ scale_x_continuous(breaks=c(0.5,1.0,1.5,2.0,2.5),labels=c("0.5","","1.5","","2.5"))
