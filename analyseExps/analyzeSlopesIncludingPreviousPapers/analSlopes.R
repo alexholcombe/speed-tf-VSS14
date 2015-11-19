cd336a129a53d9fedc68adea3f963079f481b632
Alex O. Holcombe <aoholcombe@gmail.com>
2015-11-19 16:57:48 +1100

    Working on example psychometric theoretical functions for 2 objs

--- analyseExps/analyzeSlopesIncludingPreviousPapers/analSlopes.R
+++ analyseExps/analyzeSlopesIncludingPreviousPapers/analSlopes.R
@@ -2,19 +2,45 @@
 
 #Assumes 'threshes_tf_postVSS_13targets2349objects.Rdata' and 'threshes_speed_postVSS_13targets2349objects'
 #created by doAllAnalyses_E4ab.R
-load("../data/threshes_tf_postVSS_13targets2349objects.Rdata",verbose=TRUE)
-load("../data/threshes_speed_postVSS_13targets2349objects.Rdata",verbose=TRUE)
-thrTf<-threshes_tf_postVSS_13targets2349objects; thrTf$iv<-"tf"
-thrSp<-threshes_speed_postVSS_13targets2349objects; thrSp$iv<-"speed"
+load("../data/thrAll.Rdata",verbose=TRUE)
 
-#Fittng psychometric functions with penalized likelihood should prevent extremely large slopes
-numExtremeSlopes<- sum(thrSp$slopeThisCrit < -10) #not a number
+#Fittng psychometric functions with penalized likelihood prevented extremely large slopes
+numExtremeSlopes<- sum(thrAll$slopeThisCrit < -10) #not a number
 if (is.na(numExtremeSlopes)) numExtremeSlopes<-0
 if (numExtremeSlopes) {
   writeLines( paste("Your psychometric function fitting yielded",numExtremeSlopes," extremely steep slopes"))
 }
-thr<- rbind(thrTf,thrSp)
-##########################################tf mean threshes against targets
+###Create mean psychometric function, to show lack of overlap between tf limit
+#and speed limit for objects=2
+source('helpers/psychometricHelpRobust6.R') #for makeMyPlotCurve4
+numPointsForPsychometricCurve=500
+myPlotCurve <- makeMyPlotCurve4("speed",0.03,2.5,numPointsForPsychometricCurve)
+#mean, slope, lapseRate, chanceRate, method, linkFx
+dd<- subset(thrAll,(exp=="4a"|exp=="4b") & iv=="speed" & criterionNote=="threeQuarters")
+dd<-subset(dd,numTargets==1)  
+#Find average params for each numObjects condition
+modeOfList<- function(l) { names(which.max(table(l)))[1] } #R doesn't have built-in mode function!
+avg<- dplyr::summarise( dplyr::group_by(dd, numObjects),   mean=mean(mean), numTargets=1,
+          thresh=mean(thresh),slope=mean(slope),lapseRate=mean(lapseRate),chanceRate=mean(chanceRate),
+          method=modeOfList(method), linkFx=modeOfList(linkFx) )
+#Plug in average params to get average psychometric functions
+avgPsycho<-ddply(avg,"numObjects",myPlotCurve)
+#Plot speed limits.
+#Create psychometric curve predicted by tf limit for 2-object condition. Take 9-object psychometric function, 
+#speed=tf/numObjects
+#9 object case has speeds tf/9. We want speeds of tf/2. So, multiply speeds by 9/2
+#The only problem is that chance will now be 50%. So simply rescale to that?
+tfLimFor2objs<- subset(avgPsycho,numObjects==9)
+tfLimFor2objs$speed<- tfLimFor2objs$speed*9/2
+tfLimFor2objs$type<-"tf"
+twoObjs<-subset(avgPsycho,numObjects==2)
+twoObjs$type<-"actual"
+twoObjs<-rbind(twoObjs,tfLimFor2objs)
+
+g<-ggplot(twoObjs,aes(x=speed,y=correct,color=factor(type)))
+g<-g+geom_point()
+g  
+##########################################tf mean slope against targets
 tit=paste0(expNames,"_tfSpeedSlopeMeanAgainstDistractors_",infoMsg,"_threeQuarterThresh")
 quartz(title=tit,width=6,height=3)
 #Not fair to include values above the worst-observer's lapse rate. Because then the speed limit cost of second target is infinite.
@@ -33,4 +59,26 @@ h<-h+themeAxisTitleSpaceNoGridLinesLegendBox
 h<-h+scale_x_continuous(breaks=c(3,6,9,12))
 h<-h+ggtitle(paste(tit,lapseMsg))
 show(h) #http://stackoverflow.com/questions/7455046/how-to-make-graphics-with-transparent-background-in-r-using-ggplot2?rq=1
+#None of these make the slopes particularly flat.
 ggsave( paste0('figs/',tit,'.png') ,bg="transparent" ) #bg option will be passed to png
+
+#################SLOPES##################
+# h<-ggplot(data=threshes,aes(x=separatnDeg,y=-slopeThisCrit,color=numTargets))
+# 
+# tit=paste0(expNames,"_tfMeanThreshAgainstDistractors ",infoMsg," threeQuarterThresh")
+# quartz(title=tit,width=6,height=3)
+# #Not fair to include values above the worst-observer's lapse rate. Because then the speed limit cost of second target is infinite.
+# d<-subset(threshes,criterionNote=="threeQuarters")
+# d$targets<-as.factor(d$numTargets); d$objects<-d$numObjects
+# h<-ggplot(data=d, aes(x=objects,y=tfThresh,color=targets))
+# h<-h+facet_grid(exp ~ .)  #facet_grid(criterion ~ exp)
+# h<-h+ylab('threshold (Hz)')
+# h<-h+themeAxisTitleSpaceNoGridLinesLegendBox
+# #h<-h+ coord_cartesian( xlim=c(xLims[1],xLims[2]), ylim=yLims ) #have to use coord_cartesian here instead of naked ylim()
+# h<-h+scale_x_continuous(breaks=c(3,6,9,12))
+# dodgeAmt=.25
+# h<-h+ stat_summary(fun.y=mean,geom="point",position=position_dodge(width=dodgeAmt))
+# h<-h+ stat_summary(fun.y=mean,geom="line",position=position_dodge(width=dodgeAmt))
+# h<-h+stat_summary(fun.data="mean_cl_boot",geom="errorbar",width=.25,conf.int=.95,position=position_dodge(width=dodgeAmt)) 
+# h<-h+ggtitle(paste(tit,lapseMsg))
+# show(h) #http://stack
--- analyseExps/makePlots.R
+++ analyseExps/makePlots.R
@@ -25,90 +25,7 @@ thr$objects <- as.numeric(thr$numObjects) #Otherwise can't connect with lines
 thr$targets <- as.factor(thr$numTargets) 
 d<-subset(thr,criterionNote=="threeQuarters") # & exp!="HC2013")
 d$iv<-as.factor(d$iv)
-#levels(d$iv) <- c(" ", "  ") #don't show tf, speed facet labels because implied by ylab
-al=1.0 #alpha, if want to emphasize speed points, not t.f. points
-h<-ggplot(data=d, aes(x=objects,y=thresh,color=targets,fill=iv)) 
-h<-h+facet_grid(iv ~ exp, scales="free_y") #"free_y")  
-#h<-h+themeAxisTitleSpaceNoGridLinesLegendBox
-sz=2.5
-h<-h+scale_y_continuous(breaks=seq(0,6)) #No way to set axis labels independently. Could be complicated with a custom axis labeller ?scales::trans_new
-h<-h+scale_x_continuous(breaks=seq(2,12,2))
-h<-h+theme(axis.title.y=element_text(vjust=0.4)) #Move y axis label slightly away from axis
-dodgeWidth<-0
-h<-h+ stat_summary(fun.y=mean,geom="point",position=position_dodge(width=dodgeWidth), size=2.5,shape=15)
-h<-h+ stat_summary(fun.y=mean,geom="line",position=position_dodge(width=dodgeWidth))
-h<-h+stat_summary(fun.data="mean_cl_boot",geom="errorbar",width=.25,conf.int=.95,position=position_dodge(width=dodgeWidth)) 
-h<-h+theme(axis.text=element_text(size=8), axis.title=element_text(size=10)) #default text was too big
-h<-h+ggtitle(paste("5,8 difft validates t.f. limit. Speed limits vary widely",lapseMsg))
-#h<-h+coord_cartesian(ylim=c(1.5,2.5)) #have to use coord_cartesian here instead of naked ylim() to don't lose part of threshline
-show(h) #http://stackoverflow.com/questions/7455046/how-to-make-graphics-with-transparent-background-in-r-using-ggplot2?rq=1
-ggsave( paste0('figs/',tit,'.png') ,bg="transparent" ) #bg option will be passed to png
-###Contrast only tf and speed
-#Show the speed domain with a different contrast, or
-#Maybe don't connect the 2,3 data with larger numbers of objects. And use squares instead of circles
-tit<-paste0(expNames,'_highlightSpeedDomain_MeanAgainstObjects_',"tf_speed",'_threeQuarterThresh') 
-quartz(title=tit,width=6,height=4.5) #create graph of threshes with only speed and tf iv's
-thrTfSpd<- subset(thr, iv=="speed" | iv=="tf")
-thrTfSpdMoreThan3<- subset(thrTfSpd, objects>3)
-k<-h %+% thrTfSpdMoreThan3
-k<-k+ylab('Hz                                rps    ')  #ylab('tf (Hz)        speed (rps)')
-k<-k+theme_classic()
-k<-k+theme(panel.margin.x=unit(.04, "npc"),panel.margin.y=unit(.03,"npc"))
-#Now add the <=3 objects conditions back in, as squares
-thrTfSpd2an3<- subset(thrTfSpd, objects<=3)
-#k<-k+stat_summary(data=thrTfSpd2and3, fun.y=mean,geom="point",shape=b) # Doesn't work
-#It seems I can't choose shape with stat_summary, so need to calculate mean so can use geom_point
-thrMeans<-dplyr::summarise(group_by(thrTfSpd,objects,targets,exp,iv), thresh =mean(thresh,na.rm=TRUE) )
-#Somewhat limited by speed so draw with squares.
-k<-k+geom_point(data=subset(thrMeans,objects<4), size=sz, shape=15)
-k<-k+geom_line(data=subset(thrMeans,objects<4))
-k<-k+stat_summary(data=thrTfSpd2and3,fun.data="mean_cl_boot",geom="errorbar",width=.25,conf.int=.95) #error bar has to use non-means
-#Drawing dashed line connecting to rest of data is more complicated 
-#experiment. Because HC2013 needs to connect to 6 objects
-#HC2013speedLimited<- subset(thrMeans,exp=="HC2013" & objects<
-k<-k+ stat_summary(data=thrTfSpd2and3,fun.y=mean,geom="line")
-k<-k+stat_summary(data=thrTfSpd2and3,fun.data="mean_cl_boot",geom="errorbar",width=.25,conf.int=.95) 
-#I need a larger vertical spacing between the panels. How to control vertical indepnedn
-#Draw rectangles
-topPanelYrange<-ggplot_build(k)$panel$ranges[[1]]$y.range
-btmPanelYrange<-ggplot_build(k)$panel$ranges[[5]]$y.range
-xRange<- ggplot_build(k)$panel$ranges[[1]]$x.range
-#Create separate rectangles for iv=tf and iv=speed
-tfArea<-data.frame(xmin=3.5, xmax=xRange[2], ymin=topPanelYrange[1], ymax=topPanelYrange[2], limitation="tf",iv="speed")
-tfArea<-rbind(tfArea, data.frame(
-  xmin=3.5, xmax=xRange[2], ymin=btmPanelYrange[1], ymax=btmPanelYrange[2], limitation="tf",iv="tf") )
-whiteRectToBeDroppedButToActivateLegend<- data.frame(
-  xmin=4.5,xmax=4.501,ymin=1.2,ymax=1.201,limitation="speed",iv="speed") #,
-#  xmin=-2,xmax=-1,ymin=1,ymax=2,limitation="tf",iv="tf")
-#How am I going to get rid of the whiteRect? Can't, but make it invisible by having background also be white
-rectLims<-rbind(tfArea,whiteRectToBeDroppedButToActivateLegend)
-k<-k+geom_rect(data=rectLims, aes(NULL, NULL, xmin=xmin,xmax=xmax,ymin=ymin,ymax=ymax, 
-                   fill = limitation, alpha=0.5),colour=NA)
-#Unfortunately, drawing a rectangle always then causes the axis limits to retreat so it doesn't
-#go to the border. Can't crop with coord_cartesian differently in different panels, so
-#No way around that without hacking the graph direcly by modifying its grobs and
-#then rebuilding the plot, which would take a long time to work out. Just be happy with what I got.
-k<-k+ scale_fill_manual( values = c("white","grey70") )
-k<-k+  theme(legend.key = element_rect(color="grey80")) # put boxes around legend bits
-k<-k+theme( panel.background = element_blank(),
-            strip.background = element_blank(),
-            panel.grid.minor = element_blank(),
-            strip.text.y = element_blank(), #Don't need these labels, because units imply them
-            axis.line = element_line(size=.3, color = "grey40")
-            #axis.text.x = element_text(color="grey30")
-            )
-k<-k+ guides(fill = guide_legend(title = "limited by" ))
-k<-k+ guides(alpha = FALSE ) #Eliminate the alpha legend.
-k<-k+ coord_cartesian(xlim=c(0.9,xRange[2])) #Give a little breathing space on left so points stand out more
-k
-ggsave( paste0('figs/',tit,'.png')) # ,bg="transparent" ) #bg option will be passed to png
-############################################
-####vertically arrayed. Won't work because the two columns (rps vs. Hz) need different y-axes
-#which you can't do in ggplot. Anyway, it looks like crap because the rps numbers are so much 
-#lower than the Hz numbers.
-j<-h + facet_grid(exp ~ iv, scales="free_y") #"free_y") 
-quartz(title=tit,width=3.2,height=6.3) 
-show(j)
+
 ##########Plot threshes, exp*subject*numTargets*numObjects ################
 tit=paste(expNames,"_indivSs_threshesDistractors_",infoMsg,"_threeQuarterThresh",sep='')
 quartz(title=tit,width=7,height=4) #create graph of thresholds
@@ -228,23 +145,4 @@ h<-h+ggtitle(paste(tit,lapseMsg))
 show(h) #http://stackoverflow.com/questions/7455046/how-to-make-graphics-with-transparent-background-in-r-using-ggplot2?rq=1
 ggsave( paste0('figs/',tit,'.png') ,bg="transparent" ) #bg option will be passed to png
 
-#################SLOPES##################
-# h<-ggplot(data=threshes,aes(x=separatnDeg,y=-slopeThisCrit,color=numTargets))
-# 
-# tit=paste0(expNames,"_tfMeanThreshAgainstDistractors ",infoMsg," threeQuarterThresh")
-# quartz(title=tit,width=6,height=3)
-# #Not fair to include values above the worst-observer's lapse rate. Because then the speed limit cost of second target is infinite.
-# d<-subset(threshes,criterionNote=="threeQuarters")
-# d$targets<-as.factor(d$numTargets); d$objects<-d$numObjects
-# h<-ggplot(data=d, aes(x=objects,y=tfThresh,color=targets))
-# h<-h+facet_grid(exp ~ .)  #facet_grid(criterion ~ exp)
-# h<-h+ylab('threshold (Hz)')
-# h<-h+themeAxisTitleSpaceNoGridLinesLegendBox
-# #h<-h+ coord_cartesian( xlim=c(xLims[1],xLims[2]), ylim=yLims ) #have to use coord_cartesian here instead of naked ylim()
-# h<-h+scale_x_continuous(breaks=c(3,6,9,12))
-# dodgeAmt=.25
-# h<-h+ stat_summary(fun.y=mean,geom="point",position=position_dodge(width=dodgeAmt))
-# h<-h+ stat_summary(fun.y=mean,geom="line",position=position_dodge(width=dodgeAmt))
-# h<-h+stat_summary(fun.data="mean_cl_boot",geom="errorbar",width=.25,conf.int=.95,position=position_dodge(width=dodgeAmt)) 
-# h<-h+ggtitle(paste(tit,lapseMsg))
-# show(h) #http://stack
+
